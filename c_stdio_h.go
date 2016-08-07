bd1c43e597c43f7215582ba046e40f05a31eab74
chai2010 <chaishushan@gmail.com>
2016-08-07 17:16:17 +0800

    zz

--- c_stdio_h.go
+++ c_stdio_h.go
@@ -7,10 +7,36 @@ package cgo
 //#include <stdio.h>
 import "C"
 
-type CFILE C.FILE
+type CFile C.FILE
+
+func CRemove(filename *CChar) int {
+	return int(C.remove((*C.char)(filename)))
+}
+
+func CRename(oldname, newname *CChar) int {
+	return int(C.rename((*C.char)(oldname), (*C.char)(newname)))
+}
+
+func CTmpfile() *CFile {
+	return (*CFile)(C.tmpfile())
+}
+
+func CFopen(filename, mode *CChar) *CFile {
+	return (*CFile)(C.fopen((*C.char)(filename), (*C.char)(mode)))
+}
+
+func CFclose(p *CFile) int {
+	return int(C.fclose((*C.FILE)(p)))
+}
+
+func CFflush(p *CFile) int {
+	return int(C.fflush((*C.FILE)(p)))
+}
 
 func CPuts(s *CChar) int {
 	return int(C.puts((*C.char)(s)))
 }
 
 // TODO: support <stdio.h>
+
+// TODO: rename package cgo to crt
