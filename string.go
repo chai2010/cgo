7f61fced0847a79c9dacf11bb9bd4f228b048f9e
chai2010 <chaishushan@gmail.com>
2016-08-09 12:16:10 +0800

    wrap c string to Go

---
+++ string.go
@@ -0,0 +1,76 @@
+// Copyright 2016 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package cgo
+
+//#include "./c_string.h"
+import "C"
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+type String C.chai2010_cgo_String_T
+
+func NewString() *String {
+	return (*String)(C.chai2010_cgo_String_New())
+}
+func NewStringFromStr(s *Char) *String {
+	return (*String)(C.chai2010_cgo_String_NewFromStr((*C.char)(s)))
+}
+func NewStringFromStrN(s *Char, n int) *String {
+	return (*String)(C.chai2010_cgo_String_NewFromStrN((*C.char)(s), C.int(n)))
+}
+func (s *String) Delete() {
+	C.chai2010_cgo_String_Delete((*C.chai2010_cgo_String_T)(s))
+}
+
+func (s *String) Size() int {
+	return int(C.chai2010_cgo_String_Size((*C.chai2010_cgo_String_T)(s)))
+}
+func (s *String) Resize(newSize int) {
+	C.chai2010_cgo_String_Resize((*C.chai2010_cgo_String_T)(s), C.int(newSize))
+}
+func (s *String) Clear() {
+	C.chai2010_cgo_String_Clear((*C.chai2010_cgo_String_T)(s))
+}
+
+func (s *String) Data() *Char {
+	return (*Char)(C.chai2010_cgo_String_CStr((*C.chai2010_cgo_String_T)(s)))
+}
+func (s *String) CStr() *Char {
+	return (*Char)(C.chai2010_cgo_String_CStr((*C.chai2010_cgo_String_T)(s)))
+}
+
+func (s *String) AssignStr(str *Char) {
+	C.chai2010_cgo_String_AssignStr((*C.chai2010_cgo_String_T)(s), (*C.char)(str))
+}
+func (s *String) AssignStrN(str *Char, n int) {
+	C.chai2010_cgo_String_AssignStrN((*C.chai2010_cgo_String_T)(s), (*C.char)(str), C.int(n))
+}
+
+func (s *String) AssignByGoString(str string) {
+	C.chai2010_cgo_String_AssignStrN((*C.chai2010_cgo_String_T)(s),
+		(*C.char)(unsafe.Pointer((*reflect.StringHeader)(unsafe.Pointer(&str)).Data)),
+		C.int(len(str)),
+	)
+}
+func (s *String) AssignByGoBytes(str []byte) {
+	C.chai2010_cgo_String_AssignStrN((*C.chai2010_cgo_String_T)(s),
+		(*C.char)(unsafe.Pointer((*reflect.StringHeader)(unsafe.Pointer(&str)).Data)),
+		C.int(len(str)),
+	)
+}
+
+func (s *String) GoString() string {
+	return C.GoString((*C.char)(s.Data()))
+}
+
+func (s *String) GoSlice() []byte {
+	if p, n := s.Data(), s.Size(); n > 0 {
+		return (*[1 << 30]byte)(unsafe.Pointer(p))[0:n:n]
+	}
+	return nil
+}
