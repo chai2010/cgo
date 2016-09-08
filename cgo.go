4881a422e0ce07691d4a52584e7b5689a4b522e6
chai2010 <chaishushan@gmail.com>
2016-09-09 06:15:37 +0800

    add builtin cgo helper

--- cgo.go
+++ cgo.go
@@ -12,3 +12,37 @@ package cgo
 
 */
 import "C"
+import "unsafe"
+
+// Go string to C string
+// The C string is allocated in the C heap using malloc.
+// It is the caller's responsibility to arrange for it to be
+// freed, such as by calling C.free (be sure to include stdlib.h
+// if C.free is needed).
+func CString(s string) *Char {
+	return (*Char)(C.CString(s))
+}
+
+// Go []byte slice to C array
+// The C array is allocated in the C heap using malloc.
+// It is the caller's responsibility to arrange for it to be
+// freed, such as by calling C.free (be sure to include stdlib.h
+// if C.free is needed).
+func CBytes(s []byte) unsafe.Pointer {
+	return C.CBytes(s)
+}
+
+// C string to Go string
+func GoString(s *Char) string {
+	return C.GoString((*C.char)(s))
+}
+
+// C data with explicit length to Go string
+func GoStringN(s *Char, n int) string {
+	return C.GoStringN((*C.char)(s), C.int(n))
+}
+
+// C data with explicit length to Go []byte
+func GoBytes(p unsafe.Pointer, n int) []byte {
+	return C.GoBytes(p, C.int(n))
+}
